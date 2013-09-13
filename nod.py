a1e395d576dbfaa517d1e427baeb0ae21ffa86b2
Pietro Zambelli <peter.zamb@gmail.com>
2013-09-13 18:27:46 +0100

    Go on and test

--- nod.py
+++ nod.py
@@ -5,98 +5,98 @@ import sys
 if sys.version[:3] != '2.7':
         raise SystemError
 
+PBS = """#!/bin/csh
+#-------------------------------
+#PBS -l nodes=1:ppn=8
+#PBS -o work.log
+#PBS -m abe
+#PBS -M pizamb48@ceh.ac.uk
+#PBS -q small
+#-------------------------------
 
-from grass.pygrass.modules import Module, GridModule
+#
+# SET GRASS ENVIRONMENT VARIABLES
+#
+source ~/.csh_profile
+echo "print GISRC:"
+echo $GISRC
+
+set RUNDIR = "/home/pizamb48/work/grass/20130913"
+cd $RUNDIR
 
 
-kwargs = {"cmd": "ml.nemesis",
-          "group": "rgb",
+echo "current working directory: " `pwd`
+echo "-------------------------------------------------------------------"
+echo "Job started on" `date`
+echo "-------------------------------------------------------------------"
+python {script}
+echo "-------------------------------------------------------------------"
+echo "Job ended on" `date`
+echo "-------------------------------------------------------------------"
+
+"""
+
+
+from grass.pygrass.modules.grid import GridModule
+from grass.pygrass.modules.grid import Nodes
+from grass.pygrass.gis import Mapset
+
+
+mapset = Mapset()
+
+
+kwargs = {"group": "rgb",
           "rast": ['photo_r', 'photo_g', 'photo_b'],
           "hdf": "test.hdf",
           "seg_thresholds": [0.01,0.05],
           "seg_opts": "method=region_growing,similarity=euclidean,minsize=2",
+          #"seg_name": "ortho_segs_l005",
           "data": "data",
           "datasum": "datasum",
           "training_hdf": "training.hdf",
           "training_kchk1": "Ktr",
+          "training_kchk2": "Ktr2",
           "training_ychk": "ytr",
-          "training_number" : 100,
+          "training_number" : 130,
           "training_conf": "mlconf.py",
           "training_mls": "BEST",
           "training_key": "tree",
-          "output": "test_tree_800_0"}
+          "transform": "standardize",
+          "output": "tree_rm_small2",
+          "area_size": 20,
+          "visible": ['test', 'PERMANENT'],
+          #"flags": 'r',
+          "overwrite": True}
 
+slp = {'elevation': 'elevation',
+       'slope': 'slope',
+       'aspect': 'aspect',
+       'overwrite': True}
 
-grid = {"width": 800,
-        "height": 800,
-        "overlap": 30,
-        "debug": False}
+grid = {"width": 1000,
+        "height": 1000,
+        "overlap": 100,
+        "debug": True,
+        "move": "/tmp/pizamb48",
+        "log": True}
+
 grid.update(**kwargs)
+#grid.update(**slp)
 
+node = {"nwidth": 1000,
+        "nheight": 1000,
+        "pbs_template": PBS}
 
-TEST = [(100, 100),
-        (200, 100),
-        (400, 100),
-        (800, 100),
-        ]
+node.update(**grid)
 
-RES = 0.25
+#md = GridModule('ml.nemesis', **grid)
+#md = GridModule('r.slope.aspect', **grid)
+#md.run(clean=False)
 
-NAME = "test_tree_{grid}x{grid}m_with_{overlap}m"
-
-EXPORT = """
-paper a4
-  end
-scale {scale}
-raster {name}
-grid {grid}
-  color black
-  numbers 1 black
-  width 2
-  end
-text 50% -7% grid: {width}x{height}m  overlap: {overlap}m
-  fontsize 16
-  end
-"""
-
-"""
-ps.map input=- output=test.ps -r --o << EOF
-paper a4
-  end
-scale 1:15
-raster test_tree_100_0
-grid 25
-  color black
-  numbers 1 black
-  width 2
-end
-text 50% -7% grid:{width}x{height}m overlap={overlap}
-  fontsize 16
-  end
-EOF
-"""
-
-
-#import ipdb; ipdb.set_trace()
-psm = Module('ps.map', input='-', flags='r', overwrite=True, output='ps',
-             run_=False)
-for grd, ovl in TEST:
-    grid['width'] = grd
-    grid['height'] = grd
-    grid['overlap'] = ovl
-    grid['output'] = NAME.format(grid=int(grd * RES),
-                                 overlap=int(ovl * RES))
-    g = GridModule(**grid)
-    g.run()
-    psm(output=grid['output'] + '.ps',
-        stdin_=EXPORT.format(scale='1:15',
-                             name=grid['output'],
-                             grid=grd,
-                             width=int(grd * RES),
-                             height=int(grd * RES),
-                             overlap=int(ovl * RES)),
-        run_=True)
-
-#n = node.Nodes()
-
+nd = Nodes("ml.nemesis", **node)
+import ipdb; ipdb.set_trace()
+nd.populate()
+que = nd.queue[0]
+que.create()
+que.run()
 
